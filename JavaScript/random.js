8268199e3162393b8b6cffad6bdc3ab29e59d692
Jacob Chademwiri <jchademwiri@gmail.com>
2018-09-30 19:30:36 +0200

    compleated for loop

--- JavaScript/random.js
+++ JavaScript/random.js
@@ -9,3 +9,10 @@ console.log( getRandomNumber(1, 5));
 console.log( getRandomNumber(1, 50));
 console.log( getRandomNumber(1, 100));
 console.log( getRandomNumber(200, 100));
+
+var counter = 0;
+while (counter < 10 ) {
+    var ruandom =randomNumber(6);
+    document.write(random + '')
+    counter += 1 ;
+}
