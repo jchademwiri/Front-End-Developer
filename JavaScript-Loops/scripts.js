c0ca1a1c0f9a09e99569258873447475ed92993d
Jacob Chademwiri <jchademwiri@gmail.com>
2018-09-30 12:16:55 +0200

    ALL FILES TO BE UPLOADED, THEN FIRST COMMIT BEGINS

---
+++ JavaScript-Loops/scripts.js
@@ -0,0 +1,19 @@
+
+
+var upper = '10000';
+var rundomNumber =gerRandomNumber(upper);
+var guess;
+var attempts = 0;
+
+
+function randomNumber(upper) {
+  return Math.floor( Math.random() * upper ) + 1;
+}
+/*
+ var counter = 0;
+ while (counter < 10 ) {
+     var ruandom =randomNumber(6);
+     document.write(random + '')
+     counter += 1 ;
+ }
+ */
